2324581d8243813554ee534eebcbddb88077175d
tbkizle <tbkizle@gmail.com>
2025-01-15 08:42:23 -0500

    Update Dev Setting

--- Specialization/Cata/Shadow.lua
+++ Specialization/Cata/Shadow.lua
@@ -249,9 +249,9 @@ function Priest:Shadow()
     end
 
 
-    if MaxDps.db.global.debugMode then
-        debugg()
-    end
+    --if MaxDps.db.global.debugMode then
+    --   debugg()
+    --end
 
     setSpell = nil
     ClearCDs()
